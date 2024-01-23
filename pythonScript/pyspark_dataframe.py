5995093c2c05c72d1aab3a32c9c3936c218a8a40
berhane02 <yberhane1@outlook.com>
2024-01-22 23:46:41 -0500

    add pyspark data processing

---
+++ pythonScript/pyspark_dataframe.py
@@ -0,0 +1,18 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+from awsglue.dynamicframe import DynamicFrame
+
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
