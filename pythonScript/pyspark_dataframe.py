ea2c38b83a98226251c1b3330de0f096ab4dcbd0
berhane02 <yberhane1@outlook.com>
2024-01-24 23:40:42 -0500

    update spark script with vitl

--- pythonScript/pyspark_dataframe.py
+++ pythonScript/pyspark_dataframe.py
@@ -7,49 +7,71 @@ from awsglue.job import Job
 
 from awsglue.dynamicframe import DynamicFrame
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
-## @type: DataSource
-## @args: [database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0"]
-## @return: datasource0
-## @inputs: []
-############################### Added by Darshil ###############################
+job.init(args["JOB_NAME"], args)
+
 predicate_pushdown = "region in ('ca','gb','us')"
 
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+# Script generated for node Amazon S3
+datasource0 = glueContext.create_dynamic_frame.from_options(
+    format_options={
+        "quoteChar": '"',
+        "withHeader": True,
+        "separator": ",",
+        "optimizePerformance": False,
+    },
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://data-eng-on-youtube-raw-useast2-dev/youtube/raw_statistics/"],
+        "recurse": True,
+    },
+    transformation_ctx="datasource0",
+)
 
-## @type: ApplyMapping
-## @args: [mapping = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1"]
-## @return: applymapping1
-## @inputs: [frame = datasource0]
+# Script generated for node Change Schema
 applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1")
-## @type: ResolveChoice
-## @args: [choice = "make_struct", transformation_ctx = "resolvechoice2"]
-## @return: resolvechoice2
-## @inputs: [frame = applymapping1]
-resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_struct", transformation_ctx = "resolvechoice2")
-## @type: DropNullFields
-## @args: [transformation_ctx = "dropnullfields3"]
-## @return: dropnullfields3
-## @inputs: [frame = resolvechoice2]
-dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
-## @type: DataSink
-## @args: [connection_type = "s3", connection_options = {"path": "s3://bigdata-on-youtube-cleansed-euwest1-14317621-dev/youtube/raw_statistics/"}, format = "parquet", transformation_ctx = "datasink4"]
-## @return: datasink4
-## @inputs: [frame = dropnullfields3]
+ = ApplyMapping.apply(
+    frame=datasource0,
+    mappings=[
+        ("video_id", "string", "video_id", "string"),
+        ("trending_date", "string", "trending_date", "string"),
+        ("title", "string", "title", "string"),
+        ("channel_title", "string", "channel_title", "string"),
+        ("category_id", "string", "category_id", "string"),
+        ("publish_time", "string", "publish_time", "string"),
+        ("tags", "string", "tags", "string"),
+        ("views", "bigint", "views", "bigint"),
+        ("likes", "bigint", "likes", "bigint"),
+        ("dislikes", "bigint", "dislikes", "bigint"),
+        ("comment_count", "bigint", "comment_count", "bigint"),
+        ("thumbnail_link", "string", "thumbnail_link", "string"),
+        ("comments_disabled", "string", "comments_disabled", "string"),
+        ("ratings_disabled", "string", "ratings_disabled", "string"),
+        ("video_error_or_removed", "string", "video_error_or_removed", "string"),
+        ("description", "string", "description", "string"),
+    ],
+    transformation_ctx="applymapping1",
+)
 
-############################### Added by Darshil ###############################
-#MAKE SURE YOU COPY ONLY WHAT IS NEEDED
-
-datasink1 = dropnullfields3.toDF().coalesce(1)
+datasink1 = applymapping1.toDF().coalesce(1)
 df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
 
-############################### Added by Darshil ###############################
+# Script generated for node Amazon S3
+datasink3 = glueContext.write_dynamic_frame.from_options(
+    frame=df_final_output,##ChangeSchema_node1706138576221,
+    connection_type="s3",
+    format="glueparquet",
+    connection_options={
+        "path": "s3://data-eng-on-youtube-clean-useast2-dev/youtube/raw_statistics/",
+        "partitionKeys": ["region"],
+    },
+    format_options={"compression": "snappy"},
+    transformation_ctx="datasink3",
+)
+
 job.commit()
